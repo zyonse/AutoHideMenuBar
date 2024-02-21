3b38c753677bf0fe2f854af065a009bcdee3dc69
Gavin Zyonse <gavin@zyonse.com>
2024-02-21 16:39:22 -0500

    Initial Commit

---
+++ hide-mac-menu-bar/hide_mac_menu_barApp.swift
@@ -0,0 +1,17 @@
+//
+//  hide_mac_menu_barApp.swift
+//  hide-mac-menu-bar
+//
+//  Created by Gavin Zyonse on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct hide_mac_menu_barApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
